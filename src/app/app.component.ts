0716d9df1935462260f763cb3f6e50f5b09cd9e0
Juan Antonio <jgarcab612@iescarrillo.es>
2026-01-19 15:00:40 +0100

    Casi hecho home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componente/navbar/navbar.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/componente/inicio/inicio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.scss'
+})
+export class InicioComponent {
+horaMinutos= "7h 34m"
+calorias="402"
+pasos="8028"
+}
