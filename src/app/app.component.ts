1f69804e2749728fdfb6ef8f3b1a5962be5f35a1
Juan Antonio <jgarcab612@iescarrillo.es>
2026-01-21 14:55:46 +0100

    Signin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componente/navbar/navbar.component';
 import { InicioComponent } from './componente/inicio/inicio.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "./componente/footer/footer.component";
+import { SigninComponent } from "./componente/autenticacion/signin/signin.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, RouterLink,NavbarComponent,InicioComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, NavbarComponent, InicioComponent, FooterComponent, SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
